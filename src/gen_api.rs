34a429a220bb2edcd37f9a442a6090c23f48363d
Rob Parrett <robparrett@gmail.com>
2024-08-06 15:50:25 -0700

    Use live API in release builds

--- src/gen_api.rs
+++ src/gen_api.rs
@@ -34,7 +34,9 @@ pub struct GenApiSettings {
 impl Default for GenApiSettings {
     fn default() -> Self {
         Self {
-            //host: "https://www.robparrett.com:5002".to_string(),
+            #[cfg(not(debug_assertions))]
+            host: "https://www.robparrett.com:5002".to_string(),
+            #[cfg(debug_assertions)]
             host: "http://localhost:5001".to_string(),
         }
     }
